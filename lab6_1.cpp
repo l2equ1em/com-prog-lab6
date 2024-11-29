d061e31fdd0984dd71d2995a9797a850ca8ee7b2
lawit <lawitpinkaew@gmail.com>
2024-11-29 16:12:08 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num = 1;
+    int evenCount;
+    int oddCount;
+
+    while (num != 0){
+        cout << "Enter an integer: ";
+        cin >> num;
+        if (num % 2 == 0 && num > 0) {
+            evenCount++;
+        } else if( num % 2 != 0 && num > 0) {
+            oddCount++;
+        }
+    }
+    cout << "#Even numbers = " << evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
+
     return 0;
 }
